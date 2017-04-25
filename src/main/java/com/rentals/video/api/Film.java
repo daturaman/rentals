61e07c92da0cd85451a3faaf79dff9a9d604565a
Michael Carter <mcarter@gresham-computing.com>
2017-04-25 15:57:49 +0100

    Film

---
+++ src/main/java/com/rentals/video/api/Film.java
@@ -0,0 +1,14 @@
+/*
+ * <copyright>
+ *
+ * Copyright (c) 2010-2017 Gresham Technologies plc. All rights reserved.
+ *
+ * </copyright>
+ */
+package com.rentals.video.api;
+
+/**
+ * @author mcarter
+ */
+public class Film {
+}
